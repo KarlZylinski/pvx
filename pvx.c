af1ec05a7083427a2ab717a3783463447deae207
Karl Zylinski <karl@zylinski.se>
2015-07-16 03:41:32 +0200

    Correct projection etc.

--- pvx.c
+++ pvx.c
@@ -25,11 +25,10 @@ static HWND g_window_handle;
 static HDC g_device_context;
 static HGLRC g_rendering_context;
 static GLuint g_shader;
-static unsigned g_window_width;
-static unsigned g_window_height;
 static Shape g_shapes[MAX_SHAPES];
 static unsigned g_free_shapes[MAX_SHAPES];
-
+static float g_projection_matrix[16];
+static GLuint g_model_view_projection_matrix_location;
 static const unsigned floats_per_vertex = 5;
 
 static LoadedFile load_file(const char* filename)
@@ -79,6 +78,37 @@ static GLuint load_shader(const char* vertex_source, const char* fragment_source
     return program;
 }
 
+static void set_projection_matrix(unsigned window_width, unsigned window_height)
+{
+    memset(g_projection_matrix, 0, sizeof(g_projection_matrix));
+    float near_plane = -1;
+    float far_plane = 1;
+    g_projection_matrix[0] = 2.0f / (window_width - 1.0f);
+    g_projection_matrix[5] = -2.0f / (window_height - 1.0f);
+    g_projection_matrix[10] = 2.0f / (far_plane / near_plane);
+    g_projection_matrix[12] = -1;
+    g_projection_matrix[13] = 1;
+    g_projection_matrix[14] = (near_plane + far_plane) / (near_plane - far_plane);
+    g_projection_matrix[15] = 1;
+}
+
+static LRESULT CALLBACK window_proc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch (message)
+    {
+    case WM_SIZE:
+    {
+        WORD width = LOWORD(lparam);
+        WORD height = HIWORD(lparam);
+        set_projection_matrix(width, height);
+        glViewport(0, 0, width, height);
+        return 0;
+    }
+    default:
+        return DefWindowProc(hwnd, message, wparam, lparam);
+    }
+}
+
 static void init(const char* window_title, unsigned window_width, unsigned window_height)
 {
     HINSTANCE h = GetModuleHandle(NULL);
@@ -88,6 +118,7 @@ static void init(const char* window_title, unsigned window_width, unsigned windo
     int h_border_thickness = GetSystemMetrics(SM_CXSIZEFRAME) + border_width;
     int v_border_thickness = GetSystemMetrics(SM_CYSIZEFRAME) + border_width;
     int caption_thickness = GetSystemMetrics(SM_CYCAPTION) + GetSystemMetrics(SM_CXPADDEDBORDER);
+
     PIXELFORMATDESCRIPTOR pfd =
     {
         sizeof(PIXELFORMATDESCRIPTOR),
@@ -108,11 +139,10 @@ static void init(const char* window_title, unsigned window_width, unsigned windo
         0, 0, 0
     };
 
+    set_projection_matrix(window_width, window_height);
     memset(g_free_shapes, 1, sizeof(g_free_shapes));
-    g_window_width = window_width;
-    g_window_height = window_height;
     wc.hInstance = h;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = window_proc;
     wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
     wc.lpszClassName = window_title;
     wc.style = CS_OWNDC;
@@ -133,6 +163,7 @@ static void init(const char* window_title, unsigned window_width, unsigned windo
     LoadedFile fragment_shader = load_file("fragment_shader.glsl");
     assert(fragment_shader.loaded);
     g_shader = load_shader(vertex_shader.data, fragment_shader.data);
+    g_model_view_projection_matrix_location = glGetUniformLocation(g_shader, "model_view_projection_matrix");
 }
 
 static void deinit()
@@ -185,7 +216,39 @@ static Handle add_shape(float* verts, int n)
     return handle;
 }
 
-static void draw_shape(Handle handle)
+static void mat_ident(float* out)
+{
+    memset(out, 0, 16 * sizeof(float));
+    out[0] = 1;
+    out[5] = 1;
+    out[10] = 1;
+    out[15] = 1;
+}
+
+static void mat_mul(const float* m1, const float* m2, float* out)
+{
+    out[0] = m1[0] * m2[0] + m1[1] * m2[4] + m1[2] * m2[8] + m1[3] * m2[12];
+    out[1] = m1[0] * m2[1] + m1[1] * m2[5] + m1[2] * m2[9] + m1[3] * m2[13];
+    out[2] = m1[0] * m2[2] + m1[1] * m2[6] + m1[2] * m2[10] + m1[3] * m2[14];
+    out[3] = m1[0] * m2[3] + m1[1] * m2[7] + m1[2] * m2[11] + m1[3] * m2[15];
+
+    out[4] = m1[4] * m2[0] + m1[5] * m2[4] + m1[6] * m2[8] + m1[7] * m2[12];
+    out[5] = m1[4] * m2[1] + m1[5] * m2[5] + m1[6] * m2[9] + m1[7] * m2[13];
+    out[6] = m1[4] * m2[2] + m1[5] * m2[6] + m1[6] * m2[10] + m1[7] * m2[14];
+    out[7] = m1[4] * m2[3] + m1[5] * m2[7] + m1[6] * m2[11] + m1[7] * m2[15];
+
+    out[8] = m1[8] * m2[0] + m1[9] * m2[4] + m1[10] * m2[8] + m1[11] * m2[12];
+    out[9] = m1[8] * m2[1] + m1[9] * m2[5] + m1[10] * m2[9] + m1[11] * m2[13];
+    out[10] = m1[8] * m2[2] + m1[9] * m2[6] + m1[10] * m2[10] + m1[11] * m2[14];
+    out[11] = m1[8] * m2[3] + m1[9] * m2[7] + m1[10] * m2[11] + m1[11] * m2[15];
+
+    out[12] = m1[12] * m2[0] + m1[13] * m2[4] + m1[14] * m2[8] + m1[15] * m2[12];
+    out[13] = m1[12] * m2[1] + m1[13] * m2[5] + m1[14] * m2[9] + m1[15] * m2[13];
+    out[14] = m1[12] * m2[2] + m1[13] * m2[6] + m1[14] * m2[10] + m1[15] * m2[14];
+    out[15] = m1[12] * m2[3] + m1[13] * m2[7] + m1[14] * m2[11] + m1[15] * m2[15];
+}
+
+static void draw_shape(Handle handle, float x, float y)
 {
     glUseProgram(g_shader);
     Shape* shape = g_shapes + handle;
@@ -194,6 +257,13 @@ static void draw_shape(Handle handle)
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, floats_per_vertex * sizeof(float), (void*)0);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, floats_per_vertex * sizeof(float), (void*)(2 * sizeof(float)));
+    static float model_projection_matrix[16];
+    static float model_matrix[16];
+    mat_ident(model_matrix);
+    model_matrix[12] = x;
+    model_matrix[13] = y;
+    mat_mul(model_matrix, g_projection_matrix, model_projection_matrix);
+    glUniformMatrix4fv(g_model_view_projection_matrix_location, 1, GL_FALSE, model_projection_matrix);
     glDrawArrays(GL_TRIANGLE_FAN, 0, shape->count);
 }
 
@@ -266,7 +336,9 @@ static int pvx_add_shape(lua_State* L)
 static int pvx_draw_shape(lua_State* L)
 {
     unsigned handle = luaL_checkint(L, 1);
-    draw_shape(handle);
+    float x = (float)luaL_checknumber(L, 2);
+    float y = (float)luaL_checknumber(L, 3);
+    draw_shape(handle, x, y);
     return 0;
 }
 
